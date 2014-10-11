be64ff4f771b68d95ab7aeb4b00e42fd6f871bd3
arkaris <myachkovd@mail.ru>
2014-10-11 18:07:10 +0400

    bug fix

--- www/pieces/footer.php
+++ www/pieces/footer.php
@@ -1,20 +1,24 @@
     </div><!--condent-->
     <div id="footer"> <span class="grey_text"> © 2014 Мир комнат </span> </div>
   </div><!--container-->
-</body>
 <?php
+echo '<script type="text/javascript" src="http://code.jquery.com/jquery-latest.min.js"></script>';
+echo '<script src="./js/ajax-form.js"></script>';
+/*
 switch ($_SERVER['PHP_SELF']) {
   case '/reg.php':
     //echo '<script src="/js/validator.js"></script>';
   case '/index.php':
-    echo '<script type="text/javascript" src="http://code.jquery.com/jquery-latest.min.js"></script>';
-    echo '<script src="./js/ajax-form.js"></script>';
+    //echo '<script type="text/javascript" src="http://code.jquery.com/jquery-latest.min.js"></script>';
+    //echo '<script src="./js/ajax-form.js"></script>';
     break;
   
   default:
-    //echo '<script type="text/javascript" src="http://code.jquery.com/jquery-latest.min.js"></script>';
-    //echo '<script src="/js/ajax-form.js"></script>';
+    echo '<script type="text/javascript" src="http://code.jquery.com/jquery-latest.min.js"></script>';
+    echo '<script src="./js/ajax-form.js"></script>';
     break;
 }
+*/
 ?>
+</body>
 </html>
