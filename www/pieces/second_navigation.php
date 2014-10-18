f9eb4cf2405ef8d57f245efb0c87ab61683fd1e1
arkaris <myachkovd@mail.ru>
2014-10-18 20:45:18 +0400

    up

--- www/pieces/second_navigation.php
+++ www/pieces/second_navigation.php
@@ -1,8 +1,8 @@
 <div class="second_navigation">
   <div class="tabs">
-    <div class="tab selection" data-tab="quests_tab"><a href="./">Квесты</a></div>
-    <div class="tab" data-tab="map_tab"><a href="./map.php">Карта квестов</a></div>
-    <div class="tab" data-tab="schedule_tab"><a href="./schedule.php">Расписание</a></div>
-    <div class="tab"  data-tab="rules_tab"><a href="./faq.php">Правила игры</a></div>
+    <div class="tab<?php echo $_SERVER['PHP_SELF']=='/index.php'? ' selection' : ''?>" data-tab="quests_tab"><a href="./">Квесты</a></div>
+    <div class="tab<?php echo $_SERVER['PHP_SELF']=='/map.php'? ' selection' : ''?>" data-tab="map_tab"><a href="./map.php">Карта квестов</a></div>
+    <div class="tab<?php echo $_SERVER['PHP_SELF']=='/schedule.php'? ' selection' : ''?>" data-tab="schedule_tab"><a href="./schedule.php">Расписание</a></div>
+    <div class="tab<?php echo $_SERVER['PHP_SELF']=='/faq.php'? ' selection' : ''?>"  data-tab="rules_tab"><a href="./faq.php">Правила игры</a></div>
   </div>
 </div>
